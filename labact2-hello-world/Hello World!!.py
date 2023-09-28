7d67788e54a5812b029bd7829f3024895c71a897
Wyatt Cero <145944455+wtcero@users.noreply.github.com>
2023-09-29 07:46:17 +0800

    Add files via upload

---
+++ labact2-hello-world/Hello World!!.py
@@ -0,0 +1,25 @@
+# Submitted by: Cero, Wyatt T.
+# BSCS - 1
+# CC2 - 1B
+# September 27, 2023
+
+# Variables and Input
+name = input("What is your name?")
+age = input("What is your age?")
+age = int(age)
+pi = 3.141592653589793
+modulo = 5 % 3
+# Output
+print(f"Hello, {name}!")
+print("Hello,", name)
+print("Hello, "+name)
+print("Hello,",name, sep="%%%%")
+print(f"My age next year is {age + 1}!")
+print(f"Pi value is {pi}!")
+print(f"Modulo is {modulo}!")
+print(f"Modulo is {5%3}!")
+
+name = 'Ventite'
+
+print(f"hello, is it me that you're looking for?, its {name}") 
+
